7616a6af1b3de45a00ddba03ca72f20abbfdd041
danzipie <danzipie@gmail.com>
2017-08-18 01:22:15 +0200

    and this

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
+var CentralAuth = artifacts.require("./CentralAuthority.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
+    deployer.link(ConvertLib, CentralAuth);
   deployer.deploy(MetaCoin);
+  deployer.deploy(CentralAuth);
 };
