36429aef09217519060f4e549da821ef3bbf76cf
danzipie <danzipie@gmail.com>
2017-08-17 23:48:21 +0200

    reset

--- migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./TimberTrack.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, TimberTrack);
-  deployer.deploy(TimberTrack);
-};
